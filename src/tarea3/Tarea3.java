b545426120532d413b55de749537a3fd20ab277c
W7-JUEGOS <W7-JUEGOS@KOZAC>
2017-08-07 20:14:43 -0600

    Commit Inicial

---
+++ src/tarea3/Tarea3.java
@@ -0,0 +1,18 @@
+
+package tarea3;
+
+/**
+ *
+ * @author Carlos Luna 
+ */
+public class Tarea3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+    }
+    
+}
